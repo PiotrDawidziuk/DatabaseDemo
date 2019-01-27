ca65c2850e706f87a3fcb3cc7861dbcdd6a6f22b
PiotrDawidziuk <piotr.k.dawidziuk@gmail.com>
2019-01-27 21:50:42 +0100

    events database

--- app/src/main/java/pl/apka/databasedemo/MainActivity.java
+++ app/src/main/java/pl/apka/databasedemo/MainActivity.java
@@ -1,8 +1,10 @@
 package pl.apka.databasedemo;
 
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,7 +13,20 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        SQLiteDatabase myDatabase = this.openOrCreateDatabase("Users",MODE_PRIVATE,null);
-        
+        try {
+            SQLiteDatabase sqLiteDatabase = this.openOrCreateDatabase("Events", MODE_PRIVATE,null);
+            sqLiteDatabase.execSQL("CREATE TABLE IF NOT EXISTS events (name VARCHAR, year INT(4))");
+
+            sqLiteDatabase.execSQL("INSERT INTO events (name, year) VALUES ('WWII', 1939)");
+            sqLiteDatabase.execSQL("INSERT INTO events (name, year) VALUES ('WWI', 1914)");
+            sqLiteDatabase.execSQL("INSERT INTO events (name, year) VALUES ('Moon landing', 1969)");
+
+            Cursor c = sqLiteDatabase.rawQuery("SELECT ")
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+
     }
 }
